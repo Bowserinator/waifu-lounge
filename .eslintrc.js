8298b695c4f6df6db1dc16155700889bd07580ba
Bowserinator <bowserinator@gmail.com>
2022-07-22 17:46:18 -0400

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    env: {
+        node: true
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:vue/recommended',
+        '@hellomouse/eslint-config'
+    ],
+    rules: {
+        'vue/this-in-template': 'off',
+        'vue/valid-v-slot': ['off'],
+        'vue/script-setup-uses-vars': 'off',
+
+        'no-unused-vars': 'warn',
+        'vue/no-unused-vars': 'warn',
+
+        'vue/html-indent': ['warn', 4],
+        'indent': ['warn', 4],
+
+        'vue/max-attributes-per-line': ['warn', {
+            'singleline': 4,
+            'multiline': 4
+        }]
+    }
+};
---
+++ content/test.md
@@ -0,0 +1,6 @@
+---
+title: Introduction
+---
+Learn how to use @nuxt/content.
+<!--more-->
+Full amount of content beyond the more divider.
